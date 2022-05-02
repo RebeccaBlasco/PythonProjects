237401d848de5f17d723f11f84252f97304f6404
potato-pineapple <93552051+potato-pineapple@users.noreply.github.com>
2022-05-02 09:56:13 -0700

    Delete Instagram (Download Profile Picture).py

--- Instagram (Download Profile Picture).py
+++
@@ -1,6 +0,0 @@
-#the code below allows us to Download
-#Instagram Profile Picture of a Stranger or a Friend:
-import instaloader
-ig = instaloader.Instaloader()
-dp = input("Enter Insta username : ")
-ig.download_profile(dp, profile_pic_only=True)
